0a22c5abd0b4ace2cb31a6d3ddfbff48deba14b4
Siddhanth Lathar <slathar@unix8.andrew.cmu.edu>
2018-06-21 20:28:00 -0400

    added main function, chanegd long long ->long

--- csim.c
+++ csim.c
@@ -3,19 +3,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <math.h>
 #include <cachelab.h>
-
-int main()
-{
-	return 0;
-}
 
 struct line //one line
 {
-	int valid;
-	int offset;
-	unsigned long long hit_freq;
-	unsigned long long tag;
+	long valid;
+	long offset;
+	unsigned long hit_freq;
+	unsigned long tag;
 	char *block;
 };
 
@@ -31,34 +27,34 @@ struct cache //sets make up a cache
 
 struct params
 {
-	int E;
-	int S;
-	int B;
+	long E;
+	long S;
+	long B;
 
-	int s;
-	int b;
+	long s;
+	long b;
 
-	int hits;
-	int misses;
-	int evictions;
+	long hits;
+	long misses;
+	long evictions;
 };
 
 struct min_max_indices
 {
-	int min_freq;
-	int max_freq;
-	int min_index;
-	int max_index;
+	long min_freq;
+	long max_freq;
+	long min_index;
+	long max_index;
 };
 
-struct cache make_cache(long long n_L, long long n_S, long long n_B)
+struct cache make_cache(long n_E, long n_S)
 {
 	struct cache new_cache;
 	struct set new_set;
 	struct new_line;
 
-	int l;
-	int s;
+	long l;
+	long s;
 
 	//alloc memory for cache sets
 	new_cache.sets = (struct set*)malloc(sizeof(struct set)*n_S);
@@ -72,7 +68,7 @@ struct cache make_cache(long long n_L, long long n_S, long long n_B)
 		new_cache.sets[s] = new_set;
 
 		//initialize to defaults
-		for(l = 0; l < n_L; l++)
+		for(l = 0; l < n_E; l++)
 		{
 			new_line.valid = -1;
 			new_line.tag = 0;
@@ -83,9 +79,9 @@ struct cache make_cache(long long n_L, long long n_S, long long n_B)
 	return new_cache;
 }
 
-int is_cache_full(struct cache cache_to_sim, unsigned long long set_index)
+long is_cache_full(struct cache cache_to_sim, unsigned long set_index, long E)
 {
-	int l; //line index
+	long l; //line index
 	struct set check_set = cache_to_sim.sets[set_index];
 	struct line check_line;
 
@@ -101,13 +97,13 @@ int is_cache_full(struct cache cache_to_sim, unsigned long long set_index)
 	return -1; //full
 }
 
-struct min_max_indices get_lru(struct cache cache_to_sim, unsigned long long set_index, struct min_max_indices m_m_i)
+struct min_max_indices get_lru(struct cache cache_to_sim, unsigned long set_index, struct min_max_indices m_m_i, long E)
 {
-	int l; //line index
-	int min_freq;
-	int max_freq = 0;
-	int max_index = 0;
-	int min_index = 0;
+	long l; //line index
+	long min_freq;
+	long max_freq = 0;
+	long max_index = 0;
+	long min_index = 0;
 	struct set check_set = cache_to_sim.sets[set_index];
 	struct line check_line;
 
@@ -146,27 +142,27 @@ struct min_max_indices get_lru(struct cache cache_to_sim, unsigned long long set
 
 
 
-struct params sim_cache(struct cache cache_to_sim, struct params init_params, unsigned long long in_addr)
+struct params sim_cache(struct cache cache_to_sim, struct params init_params, unsigned long in_addr)
 {
-	int E = init_params.E;
-	int S = init_params.S;
-	int B = init_params.B;
-	int s = init_params.s;
-	int b = init_params.b;
+	long E = init_params.E;
+	long S = init_params.S;
+	long B = init_params.B;
+	long s = init_params.s;
+	long b = init_params.b;
 
-	unsigned long long in_tag = in_addr >> (s + b);
+	unsigned long in_tag = in_addr >> (s + b);
 
-	unsigned long long set_index = (in_addr << (64 - s - b)) >> (64 - s);
+	unsigned long set_index = (in_addr << (64 - s - b)) >> (64 - s);
 
 	struct set check_set = cache_to_sim.sets[set_index];
 	struct line check_line;
 	struct min_max_indices m_m_i;
 
-	int l; //line index
-	int cache_full;
-	int old_hits = init_params.hits;
-	int least_recent;
-	int empty_line;
+	long l; //line index
+	long cache_full;
+	long old_hits = init_params.hits;
+	long least_recent;
+	long empty_line;
 
 
 	for(l = 0; l < E ; l++)
@@ -188,12 +184,12 @@ struct params sim_cache(struct cache cache_to_sim, struct params init_params, un
 		init_params.misses = init_params.misses + 1;
 	}
 
-	cache_full = is_cache_full(cache_to_sim, set_index);
+	cache_full = is_cache_full(cache_to_sim, set_index, E);
 
 	if(cache_full == -1)
 	{
 		//evict and write
-		m_m_i = get_lru(cache_to_sim, set_index, m_m_i);
+		m_m_i = get_lru(cache_to_sim, set_index, m_m_i, E);
 		least_recent = m_m_i.min_index;
 
 		check_set.lines[least_recent].tag = in_tag;
@@ -212,4 +208,47 @@ struct params sim_cache(struct cache cache_to_sim, struct params init_params, un
 		
 	}
 	return init_params;
+}
+
+
+int main(int argc, char **argv)
+{
+	struct params init_params;
+	struct cache cache_to_sim;
+	char *trace_file_path;
+	
+	unsigned long in_addr;
+
+	while(-1 != (opt = getopt(argc, argv, "s:E:b:t")))  //taken from rec slides s15
+	{
+		switch(opt)
+		{
+			case 's':
+				init_params.s = atoi(optarg);
+				break;
+			case 'E':
+				init_params.E = atoi(optarg);
+				break;
+			case 'b':
+				init_params.b = atoi(optarg);
+				break;
+			case 't':
+				trace_file_path = optarg;
+				break;
+			default:
+				printf("wrong args\n");
+				break;
+		}
+	}
+
+	init_params.hits = 0;
+	init_params.misses = 0;
+	init_params.evictions = 0;
+	init_params.S = pow(2.0, init_params.s);
+	init_params.B = pow(2.0, init_params.b);
+
+	cache_to_sim = make_cache(init_params.E, init_params.S);
+
+
+	return 0;
 }
